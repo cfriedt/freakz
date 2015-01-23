e57761c7ea2a5a941b55b799e37e01a3ad5eab44
Christopher Friedt <chris@mmbnetworks.com>
2015-01-23 10:50:37 -0500

    changed update_authors.sh, AUTHORS, ChangeLog

--- update_authors.sh
+++ update_authors.sh
@@ -6,4 +6,4 @@ if [ "$(grep "AC_INIT(${NAME}" configure.ac)" = "" ]; then
   echo "autogen.sh must be run in PWD of ${NAME} source"
   exit 1
 fi
-grep "^[0-9]" ChangeLog | awk '{split($0,a,"\t"); printf("%s, %s, %s\n", a[1] ,a[2], a[3]);}' | sort | sort -u -k 3.2 > AUTHORS
+grep "^[0-9]" ChangeLog | awk '{split($0,a,"\t"); printf("%s, %s, %s\n", a[1] ,a[2], a[3]);}' | sort -u > AUTHORS
